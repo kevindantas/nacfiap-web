b98218d793e530d3fd9cf5df4808587a4c25381d
Kevin-ds <kevin.ds.29@gmail.com>
2016-09-05 15:13:10 -0300

    Basic structure

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::resource('/cliente', 'ClienteController');
+
+Route::resource('/produto', 'ProdutoController');
+Route::resource('/venda', 'VendaController');
